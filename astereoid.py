7c9a188e82f0f664f9d54390b38793526aa604c0
Petteri-Joronen <iikkapetterijoronen@gmail.com>
2025-03-14 04:10:57 +0200

    added asteroid spawning

--- astereoid.py
+++ astereoid.py
@@ -1,12 +1,12 @@
 import pygame
-from circleshape import *
+from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
-        self.position_x=x
-        self.position_y=y
-    def draw(self,):
-        pygame.draw.circle(self,"white",(self.position_x,self.position_y),2)
+        super().__init__(x,y,radius)
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,"white",self.position,self.radius,2)
 
     def update(self, dt):
         self.position+=self.velocity * dt 
--- main.py
+++ main.py
@@ -2,24 +2,40 @@ import pygame
 import constants
 from player import *
 from constants import *
-
+from astereoid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock=pygame.time.Clock()
     dt=0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+   
+    Player.containers = (drawable, updatable)
+    Asteroid.containers=(asteroids,drawable,updatable)
+    AsteroidField.containers=(updatable)
+    
+    kivi_kentta= AsteroidField()
     pelajaa=Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+
+   
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pelajaa.draw(screen)
-        pelajaa.update(dt)
+        for piirra in drawable:            
+            piirra.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt=clock.tick(60)/1000
         
